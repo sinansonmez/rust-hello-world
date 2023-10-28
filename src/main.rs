277b35a3fced4fc4215010bd04d46b2c616a08de
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2023-10-28 22:45:02 +0000

    finish struct

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
-// https://doc.rust-lang.org/rust-by-example/custom_types/structs.html
+// https://doc.rust-lang.org/rust-by-example/custom_types/enum.html
+
+struct Point {
+    x: u32,
+    y: u32
+}
+
+struct Rectangle {
+    top_left: Point,
+    bottom_right: Point
+}
+
+fn rect_area(rect: Rectangle) -> u32 {
+    let result = rect.top_left.y * rect.bottom_right.x;
+    result
+}
+
 fn main() {
+    let rect = Rectangle {
+        top_left: Point { x: 0, y: 5 },
+        bottom_right: Point { x: 10, y: 5 }
+    };
     println!("-----------");
-    let xs: [i32; 4] = [1, 2, 3, 4];
-
-    for i in 0..xs.len() + 1 {
-        match xs.get(i) {
-            Some(xval) => println!("element {:?}", xval),
-            None => println!("element not found in index {}", i)
-        }
-    }
-
+    println!("area is {:?}", rect_area(rect));
     println!("-----------");
 }
